34ae2d83ea54326ae05c8e7b275c1c1ac7fc5557
Peter Williams <34144944+RomanTechnologies@users.noreply.github.com>
2023-11-18 23:46:32 +0000

    Add ItineraryViewModel

---
+++ lib/viewmodels/itinerary_viewmodel.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/flutter.dart';
+import 'models/itinerary.dart';
+
+import 'models/destination.dart';
+
+import 'models/user.dart';
+
+class ItineraryViewModel extends ChangeNotifier {
+  List<Itinerary> itineraries = [];
+
+  setUp() {
+    // Todo, set up logic for itinerary management
+  }
+}
