ed594dc0f0995bb1d986eb34bc17946871d426fb
Peter Williams <34144944+RomanTechnologies@users.noreply.github.com>
2023-11-18 23:42:17 +0000

    Add Destination model

---
+++ lib/models/destination.dart
@@ -0,0 +1,8 @@
+class Destination {
+  final String id;
+  final String name;
+  final String description;
+  final String imageUrl;
+
+  Destination({required this.id, required this.name, required this.description, required this.imageUrl});
+}
