c225b8bf1d0edfebe02361483c748990b3336aea
Peter Williams <34144944+RomanTechnologies@users.noreply.github.com>
2023-11-18 23:46:09 +0000

    Add HomeViewModel

---
+++ lib/viewmodels/home_viewmodel.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/flutter.dart';
+import 'models/user.dart';
+
+class HomeViewModel extends ChangeNotifier {
+  List<Itinerary> itineraries = [];
+
+  setUp() {
+    // Todo, set up logic for initializing the home page data
+  }
+}
