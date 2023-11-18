697d47ddc25f9b0a34de19bf0f4e192bdafb9a00
Peter Williams <34144944+RomanTechnologies@users.noreply.github.com>
2023-11-18 23:46:48 +0000

    Add DestinationViewModel

---
+++ lib/viewmodels/destination_viewmodel.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/flutter.dart';
+import 'models/destination.dart';
+
+class DestinationViewModel extends ChangeNotifier {
+  List<Destination> destinations = [];
+
+  setUp() {
+    // Todo, set up logic for destination details management
+  }
+}
