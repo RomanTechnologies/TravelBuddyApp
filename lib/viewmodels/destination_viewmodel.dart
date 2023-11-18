23a8b9a7c47a3102863f41c60e3c69fa5ac6e5d2
Peter Williams <34144944+RomanTechnologies@users.noreply.github.com>
2023-11-18 23:55:30 +0000

    Update DestinationViewModel with API integration

--- lib/viewmodels/destination_viewmodel.dart
+++ lib/viewmodels/destination_viewmodel.dart
@@ -2,12 +2,16 @@ import 'package:flutter/flutter.dart';
 import 'models/destination.dart';
 
 import 'services/firebase_service.dart';
+import '../services\/api_service.dart';
 
 class DestinationViewModel extends ChangeNotifier {
   FirebaseService _firebaseService = FirebaseService();
+  APIService _apiService = APIService();
   List<Destination> destinations = [];
 
   setUp() async {
-    // Todo, integrate with Firebase service to get destination data
+    var response = await _apiService.getDestinations();
+    destinations = response.map(Destination.fromJson).toList(as.List<Destination>);
+    notifyListeners();
   }
 }
