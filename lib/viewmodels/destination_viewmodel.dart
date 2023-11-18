4a48b32187600fbe5cc423c0d4944ffdb45b6e0d
Peter Williams <34144944+RomanTechnologies@users.noreply.github.com>
2023-11-18 23:52:43 +0000

    Update DestinationViewModel with Firebase integration

--- lib/viewmodels/destination_viewmodel.dart
+++ lib/viewmodels/destination_viewmodel.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/flutter.dart';
 import 'models/destination.dart';
 
+import 'services/firebase_service.dart';
+
 class DestinationViewModel extends ChangeNotifier {
+  FirebaseService _firebaseService = FirebaseService();
   List<Destination> destinations = [];
 
-  setUp() {
-    // Todo, set up logic for destination details management
+  setUp() async {
+    // Todo, integrate with Firebase service to get destination data
   }
 }
