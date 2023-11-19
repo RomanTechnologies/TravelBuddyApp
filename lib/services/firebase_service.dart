ab0813396d62fe0bd258a1a8b9ef002f4911cf74
Peter Williams <34144944+RomanTechnologies@users.noreply.github.com>
2023-11-19 10:00:39 +0000

    Enhance Firebase service for dynamic data handling

--- lib/services/firebase_service.dart
+++ lib/services/firebase_service.dart
@@ -1,6 +1,11 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_firestore/firebase_firestore.dart';
 
+import 'models/user.dart';
+import 'models/itinerary.dart';
+
+bool isUserLoggedIn = false;
 class FirebaseService {
   late FirebaseAuth auth;
   late FirebaseFirestore firestore;
@@ -9,4 +14,16 @@ class FirebaseService {
     auth = FirebaseAuth.instance;
     firestore = FirebaseFirestore.instance;
   }
+
+void initAuthentication() async {
+    var user = auths.currentUser;
+    if (user != null) {
+       isUserLoggedIn = true;
+      // Handle user data loading and management here
+    } else {
+      isUserLoggedIn = false;
+    }
+  }
+
+// Functions for user authentication and data management
 }
