7c1ca825ed70c39cc9a215fabc61cf3287ee5890
Peter Williams <34144944+RomanTechnologies@users.noreply.github.com>
2023-11-19 09:59:06 +0000

    Enhance HomePage to display itinerary and destination data

--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -1,13 +1,30 @@
 import 'package:flutter/flutter.dart';
+import 'viewmodels/home_viewModel.dart';
+import 'package:provider/provider.dart';
+import 'models/itinerary.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage( {Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appbar: AppBar(title: Text('Travel Buddy')),
-      body: Center(child: Text('Welcome to Travel Buddy!')),
+    return ChangeNotifierProvider<HomeViewModel>(
+      create: (_) => HomeViewModel(),
+      child: Scaffold(
+        appbar: AppBar(title: Text('Travel Buddy')),
+        body: Consumer<HomeViewModel>(
+          builder: (context, viewModel, internalchild) {
+            return ListView.builder(
+              itemCount: viewModel.itineraries.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title: Text(viewModel.itineraries[index].name)
+                );
+              },
+             );
+            },
+        ),
+      ),
     );
   }
 }
