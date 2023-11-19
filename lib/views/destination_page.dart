649330b75669dc79105fda5e27a79c42eeb62d48
Peter Williams <34144944+RomanTechnologies@users.noreply.github.com>
2023-11-19 09:47:58 +0000

    Update DestinationPage with dynamic data display

--- lib/views/destination_page.dart
+++ lib/views/destination_page.dart
@@ -1,13 +1,25 @@
 import 'package:flutter/flutter.dart';
+import 'viewmodels/destination_viewModel.dart';
 
 class DestinationPage extends StatelessWidget {
   const DestinationPage( {Key? key} ) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final DestinationViewModel vmodel = DestinationViewModel();
+    vmodel.setUp();
     return Scaffold(
       appbar: AppBar(title: Text('Destination Details')),
-      body: Center(child: Text('Explore destination details here.'))
+      body: Builder(\n        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return ListView(
+              children: vmodel.destinations.map((dest) => ListTile(title: Text(dest.name))).castWidget(),
+            );
+          } else {
+            return Center(child: CircularProgressIndicator());
+          }
+        },
+      ),
     );
   }
 }
