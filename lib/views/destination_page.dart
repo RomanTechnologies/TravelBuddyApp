14634480ac85d8e816077b7bf1363e3a9518e8bd
Peter Williams <34144944+RomanTechnologies@users.noreply.github.com>
2023-11-19 09:54:05 +0000

    Enhance DestinationPage to display dynamic API data

--- lib/views/destination_page.dart
+++ lib/views/destination_page.dart
@@ -1,24 +1,28 @@
 import 'package:flutter/flutter.dart';
 import 'viewmodels/destination_viewModel.dart';
+import 'package:provider/provider.dart';
 
 class DestinationPage extends StatelessWidget {
-  const DestinationPage( {Key? key} ) : super(key: key);
+  const DestinationPage( {Key? (key} ) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    final DestinationViewModel vmodel = DestinationViewModel();
-    vmodel.setUp();
-    return Scaffold(
-      appbar: AppBar(title: Text('Destination Details')),
-      body: Builder(\n        builder: (context, snapshot) {
-          if (snapshot.hasData) {
-            return ListView(
-              children: vmodel.destinations.map((dest) => ListTile(title: Text(dest.name))).castWidget(),
-            );
-          } else {
-            return Center(child: CircularProgressIndicator());
-          }
-        },
+    return ChangeNotifierProvider<DestinationViewModel>(
+      create: (_) => DestinationViewModel(),
+      child: Scaffold(
+        appbar: AppBar(title: Text('Destination Details')),
+        body: Consumer<DestinationViewModel>(
+          builder: (context, vmodel, internalchild) {
+            return vmodel.destinations.isEmpty
+              ? Center(child: Text('No destinations available'))
+              : ListView.builder(
+                  itemCount: vmodel.destinations.length,
+                itemBuilder: (context, index) {
+                  return ListTile(title: Text(vmodel.destinations[index].name));
+                },
+              );
+            },
+        ),
       ),
     );
   }
