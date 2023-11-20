3ed417b0037000e6c0c441a902f91fcddf53548c
Peter Williams <34144944+RomanTechnologies@users.noreply.github.com>
2023-11-20 18:50:44 +0000

    Updated HomeViewModel to integrate with FirebaseService for data handling

--- lib/viewmodels/home_viewmodel.dart
+++ lib/viewmodels/home_viewmodel.dart
@@ -1,10 +1,29 @@
 import 'package:flutter/flutter.dart';
+import 'viewmodels/base_viewmodel.dart';
+import 'models/itinerary.dart';
 import 'models/user.dart';
+import 'utils/enums.dart';
+import 'utils/logger.dart';
+import 'services/firebase_service.dart';
 
-class HomeViewModel extends ChangeNotifier {
-  List<Itinerary> itineraries = [];
+// Home ViewModel that interacts with the FirebaseService
+class HomeViewModel {
+  FirebaseService _firebaseService;
 
-  setUp() {
-    // Todo, set up logic for initializing the home page data
+  HomeViewModel() {
+    _notifyListeners = notifyListeners;
+    _firebaseService = FirebaseService();
+  }
+
+  // Initializes the viewModel by fetching data from Firebase
+  void initVoid()async {
+    try {
+      var userData = await _firebaseService.getUserData();
+      if (userData != null) {
+        // Handle user data processing and update the state
+      }
+    } catch (e) {
+      logger.e(e.toString());
+    }
   }
 }
