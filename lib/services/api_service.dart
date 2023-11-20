9b4910b26428564ddc81d97897f37ad81823d3ab
Peter Williams <34144944+RomanTechnologies@users.noreply.github.com>
2023-11-20 11:42:55 +0000

    Enhance ApiService with additional data retrieval methods

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -5,12 +5,23 @@ import 'package:http/http.dart';
 class APIService {
   final string apiUrl = 'https://travelapi.example.com/api';
 
+  // Method to fetch destinations data
   Future<List<Dynamic>> getDestinations() async {
     var response = await Http.get(Uri.parse(apiUrl + '/destinations'));
     if (response.statusCode == 200) {
       return jsonDecode(response.body) as List<Dynamic>;
     } else {
       throw Exception('Failed to load destination data');
+    }
+  }
+
+  // New method to fetch user-specific travel information
+  Future<List<Dynamic>> getUsertravelData(string userId) async {
+    var response = await Http.get(Uri.parse(apiUrl + '/users/' + userId + '/travels'));
+    if (response.statusCode == 200) {
+      return jsonDecode(response.body).map((d) => d['data']).toList() as List<Dynamic>;
+    } else {
+      throw Exception('Failed to load user travel information');
     }
   }
 }
